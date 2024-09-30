cc89cd0d01d17525bc7335d8444ff531ff3bede6
Kauan Mocelin <kauanmocelin@gmail.com>
2024-09-30 15:54:49 -0300

    create infra layer

---
+++ infrastructure/src/main/java/dev/kauanmocelin/bank/Launcher.java
@@ -0,0 +1,9 @@
+package dev.kauanmocelin.bank;
+
+import org.springframework.boot.SpringApplication;
+
+public class Launcher {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAppConfig.class, args);
+    }
+}
