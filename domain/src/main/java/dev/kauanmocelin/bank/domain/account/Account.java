ae431c1b628f6fefcfb59c45a183c592009ea5c1
Kauan Mocelin <kauanmocelin@gmail.com>
2024-10-01 17:17:35 -0300

    add transactions concept

--- domain/src/main/java/dev/kauanmocelin/bank/domain/account/Account.java
+++ domain/src/main/java/dev/kauanmocelin/bank/domain/account/Account.java
@@ -1,18 +1,35 @@
 package dev.kauanmocelin.bank.domain.account;
 
+import dev.kauanmocelin.bank.domain.transaction.Transaction;
+import dev.kauanmocelin.bank.domain.transaction.TransactionType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.List;
 import java.util.Optional;
 
-@AllArgsConstructor
 @Builder
 @Getter
 public class Account {
 
-    private final AccountId id;
+    private AccountId id;
     private Double balance;
+    private final List<Transaction> transactions;
+
+    private Account(AccountId id, Double balance, List<Transaction> transactions) {
+        this.id = id;
+        this.balance = balance;
+        this.transactions = transactions;
+    }
+
+    public static Account toSaveNew(){
+        return new Account(null, 0.0, List.of());
+    }
+
+    public static Account loadExist(AccountId id, Double balance, List<Transaction> transactions) {
+        return new Account(id, balance, transactions);
+    }
 
     public Optional<Long> getId() {
         return Optional.of(this.id.value());
@@ -22,8 +39,8 @@ public class Account {
         if (!mayWithdraw(money)) {
             return false;
         }
-
         this.balance -= money;
+        transactions.add(new Transaction(TransactionType.WITHDRAW, money));
         return true;
     }
 
@@ -33,6 +50,7 @@ public class Account {
 
     public boolean deposit(Double money) {
         this.balance += money;
+        transactions.add(new Transaction(TransactionType.DEPOSIT, money));
         return true;
     }
 }
