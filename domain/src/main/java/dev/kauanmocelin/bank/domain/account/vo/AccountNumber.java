b13b429e14069bca990cded98d07d5c1219e9f1f
Kauan Mocelin <kauanmocelin@gmail.com>
2024-10-04 19:41:26 -0300

    enhace account number features

--- domain/src/main/java/dev/kauanmocelin/bank/domain/account/vo/AccountNumber.java
+++ domain/src/main/java/dev/kauanmocelin/bank/domain/account/vo/AccountNumber.java
@@ -1,10 +1,27 @@
 package dev.kauanmocelin.bank.domain.account.vo;
 
-public record AccountNumber(Long value) {
+import java.util.Objects;
+import java.util.Random;
+
+public record AccountNumber(String value) {
 
     public AccountNumber {
-        if(value < 1) {
-            throw new IllegalArgumentException("'value' must be a positive long");
+        if(Objects.isNull(value) || value.isEmpty()) {
+            throw new IllegalArgumentException("'value' must not be null nor empty");
         }
+        if(value.length() > 8) {
+            throw new IllegalArgumentException("'value' must has maximum 8 numbers");
+        }
+        if(Long.parseLong(value) < 0) {
+            throw new IllegalArgumentException("'value' must be a positive number");
+        }
+    }
+
+    /**
+     * Generate a random identifier number with maximum 8 numbers for account number.
+     * @return create an instance of {@link AccountNumber} value
+     */
+    public static AccountNumber generate() {
+        return new AccountNumber(String.valueOf(Math.max(1, new Random().nextInt(100000000))));
     }
 }
