9b2e40f89a2d06069ac4bab0088a4339cb303a69
Kauan Mocelin <kauanmocelin@gmail.com>
2024-10-04 19:40:31 -0300

    enhace email validation

--- domain/src/main/java/dev/kauanmocelin/bank/domain/customer/vo/Email.java
+++ domain/src/main/java/dev/kauanmocelin/bank/domain/customer/vo/Email.java
@@ -1,26 +1,24 @@
 package dev.kauanmocelin.bank.domain.customer.vo;
 
 import java.util.Objects;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public record Email(String value) {
 
     private static final String EMAIL_REGEX = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
 
     public Email {
         Objects.requireNonNull(value, "'email' must not be null");
         if(value.isEmpty()) {
             throw new IllegalArgumentException("'email' must not be empty");
         }
-        if(!validate(value)) {
+        if(!isValidEmail(value)) {
             throw new IllegalArgumentException("'email' must be valid");
         }
     }
 
-    private boolean validate(String email) {
-        Pattern pattern = Pattern.compile(EMAIL_REGEX);
-        Matcher matcher = pattern.matcher(email);
-        return matcher.matches();
+    private boolean isValidEmail(final String value) {
+        return EMAIL_PATTERN.matcher(value).matches();
     }
 }
