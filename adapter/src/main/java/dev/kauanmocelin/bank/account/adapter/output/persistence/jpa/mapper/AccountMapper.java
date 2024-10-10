16717e138f35e21f96667a5c79d6d3bb42ca860f
Kauan Mocelin <kauanmocelin@gmail.com>
2024-10-10 14:34:20 -0300

    fix account mapper to transaction entity

---
+++ adapter/src/main/java/dev/kauanmocelin/bank/account/adapter/output/persistence/jpa/mapper/AccountMapper.java
@@ -0,0 +1,42 @@
+package dev.kauanmocelin.bank.account.adapter.output.persistence.jpa.mapper;
+
+import dev.kauanmocelin.bank.account.adapter.output.persistence.jpa.AccountEntity;
+import dev.kauanmocelin.bank.account.adapter.output.persistence.jpa.TransactionEntity;
+import dev.kauanmocelin.bank.domain.account.Account;
+import dev.kauanmocelin.bank.domain.account.vo.AccountNumber;
+import dev.kauanmocelin.bank.domain.transaction.Transaction;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class AccountMapper {
+
+    public Account toDomain(final AccountEntity account) {
+        return Account.builder()
+            .accountNumber(AccountNumber.from(String.valueOf(account.getAccountNumber())))
+            .balance(account.getBalance())
+            .transactions(new ArrayList<>())
+            .build();
+    }
+
+    public AccountEntity toEntity(final Account account) {
+        return AccountEntity.builder()
+            .accountNumber(Long.parseLong(account.getAccountNumber().value()))
+            .balance(account.getBalance())
+            .transactions(toTransactionEntity(account.getTransactions()))
+            .build();
+    }
+
+    private List<TransactionEntity> toTransactionEntity(final List<Transaction> transactions) {
+        return transactions.stream()
+            .map(transaction -> TransactionEntity.builder()
+                .type(transaction.getType())
+                .value(transaction.getValue())
+                .dataHora(transaction.getDataHora())
+                .build())
+            .collect(Collectors.toList());
+    }
+}
