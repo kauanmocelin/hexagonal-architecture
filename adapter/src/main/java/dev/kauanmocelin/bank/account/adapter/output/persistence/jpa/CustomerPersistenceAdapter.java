d2ac94a7743080cc58ebd9cbf7b63f498c3c7561
Kauan Mocelin <kauanmocelin@gmail.com>
2024-10-11 15:52:59 -0300

    implements customer repository for testing purpose

---
+++ adapter/src/main/java/dev/kauanmocelin/bank/account/adapter/output/persistence/jpa/CustomerPersistenceAdapter.java
@@ -0,0 +1,21 @@
+package dev.kauanmocelin.bank.account.adapter.output.persistence.jpa;
+
+import dev.kauanmocelin.bank.application.port.output.persistence.CustomerRepository;
+import dev.kauanmocelin.bank.domain.customer.Customer;
+import dev.kauanmocelin.bank.domain.customer.vo.Email;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public class CustomerPersistenceAdapter implements CustomerRepository {
+    @Override
+    public Customer createCustomer(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public Optional<Customer> loadCustomer(Email email) {
+        return Optional.empty();
+    }
+}
