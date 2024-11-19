03ac1141f3d13a6ee912c715061d48d4966f38ad
Kauan Mocelin <kauanmocelin@gmail.com>
2024-11-19 18:34:37 -0300

    convention table names

--- adapter/src/test/resources/dev/kauanmocelin/bank/account/adapter/output/persistence/AccountPersistenceAdapterTest.sql
+++ adapter/src/test/resources/dev/kauanmocelin/bank/account/adapter/output/persistence/AccountPersistenceAdapterTest.sql
@@ -1,2 +1,2 @@
-INSERT INTO account (account_number, balance) VALUES (123456, 100.00);
-INSERT INTO transaction (type, value, data_hora, account_id) VALUES ('DEPOSIT', 100.00, '2024-10-10 10:30:00', 1);
+INSERT INTO accounts (account_number, balance) VALUES (123456, 100.00);
+INSERT INTO transactions (type, transaction_value, data_hora, account_id) VALUES ('DEPOSIT', 100.00, '2024-10-10 10:30:00', 1);
